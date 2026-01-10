5ec3e2618b758a3989fa157d3c1b12aff11c4780
ajay <ajayydv7767@gmail.com>
2026-01-10 18:26:23 +0530

    SignInUpdated

--- Frontend/src/components/SIgnIn/SignIn.jsx
+++ Frontend/src/components/SIgnIn/SignIn.jsx
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const SignIn = () => {
   //submit handler
-  const submitHandler = (e)=>{
-      e.preventDefault();
-      const newData = [...userDetails];
-      newData.push({userEmail , userPassword});
-      setUserDetails(newData);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newData = [...userDetails];
+    newData.push({ userEmail, userPassword });
+    setUserDetails(newData);
 
-      setUserEmail("");
-      setUserPassword("");
-      console.log(userDetails);
-      
-  }
+    setUserEmail("");
+    setUserPassword("");
+    console.log(userDetails);
+  };
 
-
-  //for storing data 
+  //for storing data
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
-  const [userDetails,setUserDetails] = useState([])
-
-
+  const [userDetails, setUserDetails] = useState([]);
 
   return (
-    <div className=" main h-screen w-screen bg-[#000000] select-none">
-      <div className=" signin absolute top-[50%] left-[50%] -translate-[50%] bg-[#0F0F0F] p-8 rounded">
-        <div className=" logo flex items-center justify-center p-10">
-          <h1 style={{ WebkitTextStroke: "0.5px white" }} className="text-[#162942] text-5xl font-bold">
-            Lex<span style={{ WebkitTextStroke: "0.5px white" }} className="text-[#AE8623] textl-5xl font-semibold">Bridge</span>
+    <div className=" main h-screen w-screen bg-[#000000] select-none flex items-center justify-center p-10">
+      <div className="bg-[#0F0F0F] rounded flex flex-col justify-center items-center p-4">
+        <div className=" logo flex items-center justify-center p-6">
+          <h1
+            style={{ WebkitTextStroke: "0.5px white" }}
+            className="text-[#162942] text-5xl font-bold"
+          >
+            Lex
+            <span
+              style={{ WebkitTextStroke: "0.5px white" }}
+              className="text-[#AE8623] textl-5xl font-semibold"
+            >
+              Bridge
+            </span>
           </h1>
         </div>
 
@@ -40,68 +45,73 @@ const SignIn = () => {
         </div>
 
         <div className="p-2 flex flex-col gap-4 text-center">
-          <div className="w-100 h-12 bg-white flex gap-4 items-center justify-center rounded cursor-pointer active:scale-98 mt-4">
+          <div className="w-100 h-12 bg-[#181A1B] text-white flex gap-4 items-center justify-center rounded cursor-pointer active:scale-98 mt-4">
             <img
-              className="w-8 h-8"
+              className="w-6 h-6"
               src="../../src/assets/Images/google.png"
               alt=""
             />
-            <h1 className="font-medium text-xl ">Sign in with Google</h1>
+            <h1 className="font-normal text-l ">Sign in with Google</h1>
           </div>
           <div className="flex text-white mt-2">
-            <div className="h-1 w-40">------------------------</div>
+            <div className="h-1 w-40">----------------------</div>
             <p className="pl-3">or</p>
-            <div className="h-1 w-60">----------------------------</div>
+            <div className="h-1 w-60">--------------------------</div>
           </div>
         </div>
 
         <div className="text-white flex flex-col gap-4 ">
-          <form onSubmit={(e)=>{
-              submitHandler(e)
-          }} className="text-[#444242]">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-l font-medium">Email Address</h1>       
-              <input onChange={(dets)=>{
-                  setUserEmail(dets.target.value)
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+            className="text-[#444242]"
+          >
+            <div className="flex flex-col gap-2 mt-4">
+              <h1 className="text-l font-medium text-white">Email Address</h1>
+
+              <input
+                onChange={(dets) => {
+                  setUserEmail(dets.target.value);
                   console.log(dets.target.value);
-                  
-              }}  
-                className="w-100 h-10 border border-white rounded text-medium text-white font-medium p-2"
+                }}
+                className="w-100 h-10 rounded text-[15px] text-white font-medium p-2 bg-[#181A1B] shadow-xs shadow-[#5a5656]"
                 type="text"
                 placeholder="johndoe123@gmail.com"
                 value={userEmail}
               />
-          </div>
-          <div className="flex flex-col gap-2 w-100 mt-2">
-            <div className="flex items-center justify-between">
-              <h1 className="text-l font-semibold">Password</h1>
-              <h1 className="text-l font-semibold text-blue-800 cursor-pointer">
-                Forgot Password
-              </h1>
             </div>
+            <div className="flex flex-col gap-2 w-100 mt-4">
+              <div className="flex items-center justify-between">
+                <h1 className="text-l font-semibold text-white">Password</h1>
+                <h1 className="text-l font-semibold text-blue-800 cursor-pointer">
+                  Forgot Password
+                </h1>
+              </div>
 
-              <input onChange={(dets)=>{
-                setUserPassword(dets.target.value)
-              }}  
-                className="w-100 h-10 border border-white rounded text-medium text-white font-medium p-2"
+              <input
+                onChange={(dets) => {
+                  setUserPassword(dets.target.value);
+                }}
+                className="w-100 h-10 rounded text-[15px] text-white font-medium p-2 bg-[#181A1B] shadow-xs shadow-[#5a5656]"
                 type="text"
-                placeholder="Create a strong password min 8 characters"
+                placeholder="Enter your password"
                 value={userPassword}
               />
 
-            <button className="px-30 py-3 bg-blue-700 rounded text-white text-l font-semibold mt-4 cursor-pointer ">
-              Login
-            </button>
+              <button className="px-30 py-3 bg-blue-700 rounded text-white text-l font-semibold mt-4 cursor-pointer ">
+                Login
+              </button>
 
-            <h1 className="text-gray-600 font-medium text-l text-center">
-              Don't have an account yet?{" "}
-              <span className="text-l font-bold text-blue-600 cursor-pointer ">
-              <NavLink to="/signup">Create New Account</NavLink> 
-              </span>
-            </h1>
-          </div>
+              <h1 className="text-gray-600 font-medium text-l text-center">
+                Don't have an account yet?{" "}
+                <span className="text-l font-semibold text-blue-600 cursor-pointer mt-2 ">
+                  <NavLink to="/signup">Create New Account</NavLink>
+                </span>
+              </h1>
+            </div>
           </form>
-        </div>        
+        </div>
       </div>
     </div>
   );
--- Frontend/src/components/Signup/Signup.jsx
+++ Frontend/src/components/Signup/Signup.jsx
@@ -3,8 +3,8 @@ import {NavLink} from 'react-router-dom'
 
 const SignIn = () => {
   return (
-    <div className=" main h-screen w-screen bg-[#000000] select-none">
-      <div className=" signin absolute top-[50%] left-[50%] -translate-[50%] bg-[#0F0F0F] rounded p-8">
+    <div className="main h-screen w-screen bg-[#000000] select-none flex items-center justify-center p-10">
+      <div className=" bg-[#0F0F0F] rounded flex flex-col justify-center items-center p-4">
         <div className=" logo flex items-center justify-center p-4">
           <h1 style={{ WebkitTextStroke: "0.5px white" }} className="text-[#162942] text-3xl font-bold">
             Lex<span style={{ WebkitTextStroke: "0.5px white" }} className="text-[#AE8623] textl-3xl font-semibold">Bridge</span>
@@ -19,16 +19,16 @@ const SignIn = () => {
         </div>
 
         <div className="p-2 flex flex-col gap-4 text-center">
-          <div className="w-100 h-12 bg-white flex gap-4 items-center justify-center rounded cursor-pointer active:scale-98 mt-2">
+          <div className="w-100 h-12 bg-[#181A1B] text-white flex gap-4 items-center justify-center rounded cursor-pointer active:scale-98 mt-4">
             <img
-              className="w-8 h-8"
+              className="w-6 h-6"
               src="../../src/assets/Images/google.png"
               alt=""
             />
-            <h1 className="font-medium text-xl ">Sign in with Google</h1>
+            <h1 className="font-normal text-l">Sign in with Google</h1>
           </div>
           <div className="flex text-white mt-2">
-            <div className="h-1 w-40">------------------------</div>
+            <div className="h-1 w-40">----------------------</div>
             <p className="pl-3">or</p>
             <div className="h-1 w-60">----------------------------</div>
           </div>
@@ -39,7 +39,7 @@ const SignIn = () => {
           <div className="flex flex-col gap-2">
             <h1 className="text-l font-medium text-white">Email Address</h1>       
               <input
-                className="w-100 h-10 border border-white rounded text-medium text-white font-medium p-2"
+                className="w-100 h-10 bg-[#181A1B] rounded text-[15px] text-white font-medium p-4 shadow-xs shadow-[#5a5656]"
                 type="text"
                 placeholder="johndoe123@gmail.com"
               />
@@ -47,7 +47,7 @@ const SignIn = () => {
           <div className="flex flex-col gap-2 mt-2">
             <h1 className="text-l font-medium text-white">Name</h1>       
               <input
-                className="w-100 h-10 border border-white rounded text-medium text-white font-medium p-2"
+                className="w-100 h-10 bg-[#181A1B] rounded text-[15px] text-white font-medium p-4 shadow-xs shadow-[#5a5656]"
                 type="text"
                 placeholder="John Doe"
               />
@@ -55,19 +55,19 @@ const SignIn = () => {
           <div className="flex flex-col gap-2 w-100 mt-2">
               <h1 className="text-l font-semibold text-white">Create Password</h1>
               <input
-                className="w-100 h-10 border border-white rounded text-medium text-white font-medium p-1"
+                className="w-100 h-10 bg-[#181A1B] rounded text-[15px] text-white font-medium p-4 shadow-xs shadow-[#5a5656]"
                 type="text"
-                placeholder="Create a strong password min 8 characters"
+                placeholder="Create password"
               />
 
             <button className="px-30 py-2 bg-blue-700 rounded text-white text-l font-semibold mt-4 cursor-pointer ">
-              Login
+              Create Account
             </button>
 
             <h1 className="text-gray-600 font-medium text-l text-center">
                 Already have an account?{" "}
-              <span className="text-l font-bold text-blue-600 cursor-pointer ">
-              <NavLink to="/signin">login Here</NavLink> 
+              <span className="text-l font-semibold text-blue-600 cursor-pointer ">
+              <NavLink to="/signin">Login Here</NavLink> 
               </span>
             </h1>
           </div>
