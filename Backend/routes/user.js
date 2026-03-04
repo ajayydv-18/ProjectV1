3a2eddcacd81fbee0ca4d5ab105adc988aa72d4d
Shubham Yadav <ry957933@gmail.com>
2026-03-04 10:27:05 +0530

    logout logic

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,12 +2,13 @@ import express from "express";
 const router = express.Router();
 import User from '../models/user.js'
 import { createSecretToken } from "../utils/createToken.js";
-import {signup , login} from "../controllers/user.js";
+import {signup , login , logout} from "../controllers/user.js";
 import { checkAuth } from "../controllers/user.js";
 
 
 router.post("/signup" , signup);
 router.post("/login" , login);
+router.post("/logout" , logout);
 router.get('/checkAuth' , checkAuth);
 
 
