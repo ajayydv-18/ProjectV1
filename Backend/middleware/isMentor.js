673aa4e057523eb2a2ef1fef20777c2491df93e1
Shubham Yadav <ry957933@gmail.com>
2026-03-05 12:52:04 +0530

    push

--- Backend/middleware/isMentor.js
+++ Backend/middleware/isMentor.js
@@ -1,4 +1,4 @@
-import User from "../models/user";
+import User from "../models/user.js";
 
 export const isMentor = async (req, res, next) => {
     try {
--- Backend/routes/mentor.js
+++ Backend/routes/mentor.js
@@ -4,6 +4,6 @@ import User from '../models/user.js'
 import { createSecretToken } from "../utils/createToken.js";
 import { isMentor } from "../middleware/isMentor.js";
 
-router.post("/login" , login);
+// router.post("/login" , login);
 
 export default router;
